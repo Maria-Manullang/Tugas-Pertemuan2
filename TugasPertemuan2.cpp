62e28896d102a157d989a2d69335a84805dee9e8
Maria-Manullang <mariaanastasyamanullang@gmail.com>
2019-06-29 22:23:14 +0700

    kelas Programming Master

---
+++ TugasPertemuan2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+int main (){
+system("color E");
+float a,b ;
+
+cout<<"Masukkan Bilangan 1 : ";cin>>a;
+cout<<"Masukkan Bilangan 2 : ";cin>>b;
+cout <<"=============================="<<endl;
+
+cout<<"\nHasil penjumlahan dari "<<a<<" + "<<b<<" = "<<a+b;
+cout<<"\n\nHasil pengurangan dari "<<a<<" - "<<b<<" = "<<a-b;
+cout<<"\n\nHasil perkalian dari "<<a<<" * "<<b<<" = "<<a*b;
+cout<<"\n\nHasil pembagian dari "<<a<<" : "<<b<<" = "<<a/b;
+
+
+cout <<"\n========================================="<<endl;
+return 0;
+}
